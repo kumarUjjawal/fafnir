46cd341a7439ff25eecb6c3e06e6d194e1e90015
kumarUjjawal <ujjawalpathak6@gmail.com>
2023-06-13 20:18:21 +0530

    clear screen

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,6 @@
-use std::io::{self, stdout, Read};
-use termion::raw::IntoRawMode;
-fn main() {
-    let stdin = io::stdin();
-    let bytes = stdin.bytes();
-    let _stdout = stdout().into_raw_mode().unwrap();
-    for b in bytes {
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control() {
-            println!("{:?}\r", b);
-        } else {
-            println!("{:?} ({}) \r", b, c);
-        }
+mod editor;
+use editor::Editor;
 
-        if c == 'q' {
-            break;
-        }
-    }
+fn main() {
+    Editor::default().run();
 }
